c64100a5ff8f4eb705114dac4c26c1cdcc0cebb1
ishan <ishanmengi2000@gmail.com>
2023-01-10 19:01:41 +0530

    yaa

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+async function fun1(){
+    console.log("a");
+    console.log("b");
+
+    await new Promise((resolve,reject)=>{
+        console.log("c");
+        resolve();
+    },1000);
+
+    await new Promise((resolve,reject)=>{
+        console.log("d")
+        resolve();
+    },0);
+    console.log("e");
+}
+fun1();
