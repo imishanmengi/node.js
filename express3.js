c9c212220cb9670e4681338def1de6ef8e8d3b66
ishan <ishanmengi2000@gmail.com>
2023-01-17 15:14:15 +0530

    yes

---
+++ express3.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/house',(req,res,next)=>{
+    res.send('<form action="/product" method= "POST"><input type="text" name="title"><button type = "submit"> Add Product</button></form>');
+    
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+    console.log("in next middleware");
+    res.send('<h1> hello from express js</h1>');
+
+});
+
+
+
+
+app.listen(2000);
