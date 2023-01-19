302c9522bcfe6b1465d0efe7c4280195c12a1b62
ishan <ishanmengi2000@gmail.com>
2023-01-19 15:11:34 +0530

    five

--- express4.js
+++ express4.js
@@ -7,15 +7,16 @@ const shopRoutes=require('./routes/shop')
 
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1> page not found </h1>');
+
 })
 
 
 
 
 
-app.listen(20000);
+app.listen(47000);
