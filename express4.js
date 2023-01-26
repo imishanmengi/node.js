9f95427c6c148d59d0ec8ba3ba4ece469f524781
ishan <ishanmengi2000@gmail.com>
2023-01-26 15:26:30 +0530

    now

--- express4.js
+++ express4.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const db = require('./util/database');
 
 const adminRoutes= require('./routes/admin')
 const shopRoutes=require('./routes/shop')
+const contactusRoutes=require('./routes/contactus')
 
+db.execute('SELECT * FROM products')
+.then((result)=>{
+    console.log(result[0]);
+})
+.catch(err=>{
+    console.log(err);
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/admin',adminRoutes);
+app.use('/house',adminRoutes);
 app.use(shopRoutes);
+app.use(contactusRoutes);
+
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1> page not found </h1>');
@@ -19,4 +30,4 @@ app.use((req,res,next)=>{
 
 
 
-app.listen(47000);
+app.listen(34000);
