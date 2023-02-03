2ef88617f86a2f49feb1f27d371cfb94ef765afe
ishan <ishanmengi2000@gmail.com>
2023-02-03 15:08:34 +0530

    u

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql= require('mysql2');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database:'node-complete',
-    password:'Ishan@01'
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete','root','Ishan@01', {
+    dialect: 'mysql',
+    host:'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
